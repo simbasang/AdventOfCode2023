5086d9bbaf2f58fd8a50459bab4b52c3257e5153
Sebastian englund <sebastian.englund@live.se>
2023-12-02 20:50:37 +0100

    Addin first sulotion to day 1 and 2

---
+++ Day1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+var inputs = File.ReadAllLines("input.txt");
+
+const string pattern = @"\d|one|two|three|four|five|six|seven|eight|nine";
+
+var total = inputs
+    .Select(input =>
+    {
+        var first = Regex.Match(input, pattern, RegexOptions.IgnoreCase).Value;
+        var last = Regex.Match(input, pattern, RegexOptions.RightToLeft).Value;
+
+        if (first.Length > 1)
+            first = TranslateToNumber(first);
+        if (last.Length > 1)
+            last = TranslateToNumber(last);
+
+        return int.Parse(first + last);
+    }).Sum();
+
+static string TranslateToNumber(string input)
+{
+    return input switch
+    {
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9",
+        _ => ""
+    };
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var inputs = File.ReadAllLines("input.txt");
+
+var resOne = PartOne(inputs);
+var resTwo = PartTwo(inputs);
+
+static int PartOne(string[] inputs)
+{
+    var colorAmountMax = new Dictionary<string, int>()
+    {
+        { "red", 12 },
+        { "green", 13 },
+        { "blue", 14 },
+    };
+
+    //convert to linq?
+    var sum = 0;
+    foreach (var input in inputs)
+    {
+        var game = input.Split(":").Last();
+        var gameId = input.Split(":").First().Split(" ").Last();
+        var turns = game.Split(";");
+        if (turns
+            .Select(x => x.Trim().Split(","))
+            .Any(x =>
+                x.Any(
+                    z => int.Parse(z.Trim().Split(" ").First()) >
+                         colorAmountMax[z.Trim().Split(" ").Last()])
+            ))
+            continue;
+
+        sum += int.Parse(gameId);
+    }
+
+    return sum;
+}
+
+static int PartTwo(string[] inputs)
+{
+   var sum = 0;
+   //convert to linq?
+    foreach (var input in inputs)
+    {
+        var game = input.Split(":").Last();
+        var gameId = input.Split(":").First().Split(" ").Last();
+        var turns = game.Split(";");
+        var colorAmount = turns
+            .Select(x => x.Trim().Split(","));
+
+
+        var red = colorAmount.SelectMany(x =>
+            x.Where(z => 
+                    z.Trim().Split(" ").Last() == "red")
+                    .Select(z => int.Parse(z.Trim().Split(" ").First())))
+            .Max();
+        
+        var green = colorAmount.SelectMany(x =>
+            x.Where(z => 
+                    z.Trim().Split(" ").Last() == "green")
+                    .Select(z => int.Parse(z.Trim().Split(" ").First()))).Max()
+            ;
+        
+        var blue = colorAmount.SelectMany(x =>
+            x.Where(z => 
+                z.Trim().Split(" ").Last() == "blue")
+                .Select(z => int.Parse(z.Trim().Split(" ").First())))
+            .Max();
+
+        sum += (red*green*blue);
+    }
+
+    return sum;
+}
